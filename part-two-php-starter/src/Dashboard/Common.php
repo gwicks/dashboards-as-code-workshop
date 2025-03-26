428f6b1b03b19aa6c92db045941a6ea4c95ce385
Kévin Gomez <kevin.gomez@grafana.com>
2025-03-26 01:01:24 +0100

    Cleanup panels definitions in PHP

--- part-two-php-starter/src/Dashboard/Common.php
+++ part-two-php-starter/src/Dashboard/Common.php
@@ -2,235 +2,221 @@
 
 namespace App\Dashboard;
 
-use Grafana\Foundation\Common\AxisPlacement;
-use Grafana\Foundation\Common\BigValueColorMode;
-use Grafana\Foundation\Common\BigValueGraphMode;
-use Grafana\Foundation\Common\BigValueJustifyMode;
-use Grafana\Foundation\Common\BigValueTextMode;
-use Grafana\Foundation\Common\GraphGradientMode;
-use Grafana\Foundation\Common\GraphThresholdsStyleConfigBuilder;
-use Grafana\Foundation\Common\GraphThresholdsStyleMode;
-use Grafana\Foundation\Common\HeatmapCellLayout;
-use Grafana\Foundation\Common\HideSeriesConfigBuilder;
+use App\Catalog\Service;
+
+use Grafana\Foundation\Common\GraphDrawStyle;
 use Grafana\Foundation\Common\LegendDisplayMode;
 use Grafana\Foundation\Common\LegendPlacement;
-use Grafana\Foundation\Common\LogsSortOrder;
-use Grafana\Foundation\Common\ScaleDistribution;
-use Grafana\Foundation\Common\ScaleDistributionConfigBuilder;
-use Grafana\Foundation\Common\SortOrder;
+use Grafana\Foundation\Common\ReduceDataOptionsBuilder;
+use Grafana\Foundation\Common\StackingConfigBuilder;
+use Grafana\Foundation\Common\StackingMode;
 use Grafana\Foundation\Common\TooltipDisplayMode;
-use Grafana\Foundation\Common\VisibilityMode;
 use Grafana\Foundation\Common\VizLegendOptionsBuilder;
-use Grafana\Foundation\Common\VizOrientation;
 use Grafana\Foundation\Common\VizTooltipOptionsBuilder;
-use Grafana\Foundation\Dashboard\DataSourceRef;
-use Grafana\Foundation\Dashboard\FieldColorBuilder;
-use Grafana\Foundation\Dashboard\FieldColorModeId;
-use Grafana\Foundation\Dashboard\ThresholdsConfigBuilder;
-use Grafana\Foundation\Dashboard\ThresholdsMode;
+use Grafana\Foundation\Dashboard as SDKDashboard;
+use Grafana\Foundation\Dashboard\DashboardBuilder;
+use Grafana\Foundation\Dashboard\DashboardCursorSync;
+use Grafana\Foundation\Dashboard\DashboardLinkBuilder;
+use Grafana\Foundation\Dashboard\DynamicConfigValue;
+use Grafana\Foundation\Dashboard\RowBuilder;
+use Grafana\Foundation\Dashboard\TextBoxVariableBuilder;
+use Grafana\Foundation\Dashboard\VariableRefresh;
 use Grafana\Foundation\Heatmap;
-use Grafana\Foundation\Heatmap\CellValuesBuilder;
-use Grafana\Foundation\Heatmap\FilterValueRangeBuilder;
-use Grafana\Foundation\Heatmap\HeatmapColorMode;
-use Grafana\Foundation\Heatmap\HeatmapColorOptionsBuilder;
-use Grafana\Foundation\Heatmap\HeatmapColorScale;
-use Grafana\Foundation\Heatmap\RowsHeatmapOptionsBuilder;
-use Grafana\Foundation\Heatmap\YAxisConfigBuilder;
 use Grafana\Foundation\Logs;
-use Grafana\Foundation\Loki;
-use Grafana\Foundation\Prometheus;
 use Grafana\Foundation\Prometheus\PromQueryFormat;
 use Grafana\Foundation\Stat;
 use Grafana\Foundation\Text;
-use Grafana\Foundation\Text\TextMode;
 use Grafana\Foundation\Timeseries;
 
-class Common
+class Overview
 {
-    /**
-     * Returns a reference to the Loki datasource used by the
-     * workshop stack.
-     * @return DataSourceRef
-     */
-    public static function lokiDatasourceRef(): DataSourceRef
+    public static function forService(Service $service): SDKDashboard\Dashboard
     {
-        return new DataSourceRef(
-            type: 'loki',
-            uid: 'loki',
-        );
+        $builder = (new DashboardBuilder(title: $service->name.' service overview'))
+            ->uid($service->name.'-overview')
+            ->tags([$service->name, 'generated'])
+            ->readonly()
+            ->time('now-30m', 'now')
+            ->tooltip(DashboardCursorSync::crosshair())
+            ->refresh('10s')
+            ->link(
+                (new DashboardLinkBuilder('GitHub Repository'))
+                    ->url($service->repositoryUrl)
+                    ->targetBlank(true)
+            )
+            ->withVariable(
+                (new TextBoxVariableBuilder('logs_filter'))
+                    ->label('Logs filter')
+            )
+            ->withVariable(self::logLevelsVariable($service))
+        ;
+
+	    // Overview
+        $builder
+            ->withPanel(self::versionStat($service)->height(4)->span(4))
+            ->withPanel(self::descriptionText($service)->height(4)->span(4))
+            ->withPanel(self::logsVolumeTimeseries($service)->height(4)->span(16))
+        ;
+
+	    // gRPC row, if relevant
+        if ($service->hasGrpc) {
+            $builder
+                ->withRow(new RowBuilder('gRPC'))
+                ->withPanel(self::grpcRequestsTimeseries($service)->height(8))
+                ->withPanel(self::grpcLatenciesHeatmap($service)->height(8))
+                ->withPanel(self::grpcLogsPanel($service)->height(8)->span(24))
+            ;
+        }
+
+	    // HTTP row, if relevant
+        if ($service->hasHttp) {
+            $builder
+                ->withRow(new RowBuilder('HTTP'))
+                ->withPanel(self::httpRequestsTimeseries($service)->height(8))
+                ->withPanel(self::httpLatenciesHeatmap($service)->height(8))
+                ->withPanel(self::httpLogsPanel($service)->height(8)->span(24))
+            ;
+        }
+
+        return $builder->build();
     }
 
-    /**
-     * Returns a reference to the Prometheus datasource used by the
-     * workshop stack.
-     * @return DataSourceRef
-     */
-    public static function prometheusDatasourceRef(): DataSourceRef
+    private static function versionStat(Service $service): Stat\PanelBuilder
     {
-        return new DataSourceRef(
-            type: 'prometheus',
-            uid: 'prometheus',
-        );
-    }
-
-    public static function statPanel(): Stat\PanelBuilder
-    {
-        return (new Stat\PanelBuilder())
-            ->colorScheme((new FieldColorBuilder())->mode(FieldColorModeId::thresholds()))
-            ->graphMode(BigValueGraphMode::area())
-            ->colorMode(BigValueColorMode::value())
-            ->justifyMode(BigValueJustifyMode::auto())
-            ->textMode(BigValueTextMode::auto())
-            ->orientation(VizOrientation::auto())
-            ->thresholds(
-                (new ThresholdsConfigBuilder())
-                    ->mode(ThresholdsMode::absolute())
-                    ->steps([
-                        ['color' => 'green'],
-                        ['value' => 80, 'color' => 'red'],
-                    ])
+        return Common::statPanel()
+            ->title('Version')
+            ->withTarget(
+                Common::instantPrometheusQuery("app_infos{service=\"$service->name\"}")
+            )
+            ->transparent(true)
+            ->datasource(Common::prometheusDatasourceRef())
+            ->reduceOptions(
+                (new ReduceDataOptionsBuilder())
+                ->values(false)
+                ->calcs(['last'])
+                ->fields('/^version$/')
             )
         ;
     }
 
-    /**
-     * Creates a pre-configured heatmap panel.
-     * @return Heatmap\PanelBuilder
-     */
-    public static function heatmapPanel(): Heatmap\PanelBuilder
+    private static function descriptionText(Service $service): Text\PanelBuilder
     {
-        return (new Heatmap\PanelBuilder())
-            ->color(
-                (new HeatmapColorOptionsBuilder())
-                ->mode(HeatmapColorMode::scheme())
-                ->scheme('RdYlBu')
-                ->fill('dark-orange')
-                ->scale(HeatmapColorScale::exponential())
-                ->exponent(0.5)
-                ->steps(64)
-                ->reverse(false)
-            )
-            ->filterValues((new FilterValueRangeBuilder())->le(1e-09))
-            ->rowsFrame((new RowsHeatmapOptionsBuilder())->layout(HeatmapCellLayout::auto()))
-            ->showValue(VisibilityMode::auto())
-            ->cellValues(new CellValuesBuilder())
-            ->yAxis(
-                (new YAxisConfigBuilder())
-                    ->unit('s')
-                    ->reverse(false)
-                    ->axisPlacement(AxisPlacement::left())
-            )
-            ->showLegend()
-            ->mode(TooltipDisplayMode::single())
-            ->hideYHistogram()
-            ->showColorScale(true)
-            ->scaleDistribution((new ScaleDistributionConfigBuilder())->type(ScaleDistribution::linear()))
-            ->hideFrom(
-                (new HideSeriesConfigBuilder())
-                    ->tooltip(false)
-                    ->legend(false)
-                    ->viz(false)
-            )
+        return Common::textPanel($service->description)
+            ->transparent(true)
         ;
     }
 
-    /**
-     * Creates a pre-configured logs panel.
-     * @return Logs\PanelBuilder
-     */
-    public static function logPanel(): Logs\PanelBuilder
+    private static function logsVolumeTimeseries(Service $service): Timeseries\PanelBuilder
     {
-        return (new Logs\PanelBuilder())
-            ->datasource(self::lokiDatasourceRef())
-            ->showTime(true)
-            ->sortOrder(LogsSortOrder::descending())
-            ->enableLogDetails(true)
-        ;
-    }
-
-    /**
-     * Creates a text panel pre-configured for markdown content.
-     * @param string $content
-     * @return Text\PanelBuilder
-     */
-    public static function textPanel(string $content): Text\PanelBuilder
-    {
-        return (new Text\PanelBuilder())
-            ->mode(TextMode::markdown())
-            ->content($content)
-        ;
-    }
-
-    /**
-     * Creates a pre-configured timeseries panel.
-     * @return Timeseries\PanelBuilder
-     */
-    public static function timeseriesPanel(): Timeseries\PanelBuilder
-    {
-        return (new Timeseries\PanelBuilder())
-            ->lineWidth(1)
-            ->pointSize(5)
-            ->fillOpacity(20)
-            ->gradientMode(GraphGradientMode::opacity())
+        return Common::timeseriesPanel()
+            ->title('Logs volume')
+            ->withTarget(
+                Common::lokiQuery("sum by (level) (count_over_time({service=\"$service->name\", level=~\"\$logs_level\"} |~ \"\$logs_filter\" [\$__auto]))")
+                    ->legendFormat('{{ level }}')
+            )
+            ->stacking((new StackingConfigBuilder())->mode(StackingMode::normal()))
+            ->transparent(true)
+            ->datasource(Common::lokiDatasourceRef())
+            ->tooltip((new VizTooltipOptionsBuilder())->mode(TooltipDisplayMode::multi()))
             ->legend(
                 (new VizLegendOptionsBuilder())
                     ->displayMode(LegendDisplayMode::list())
-                    ->placement(LegendPlacement::bottom())
+                    ->placement(LegendPlacement::right())
                     ->showLegend(true)
             )
-            ->tooltip(
-                (new VizTooltipOptionsBuilder())
-                    ->mode(TooltipDisplayMode::single())
-                    ->sort(SortOrder::none())
+            ->drawStyle(GraphDrawStyle::bars())
+            ->overrideByName('INFO', [
+                new DynamicConfigValue('color', ['mode' => 'fixed', 'fixedColor' => 'green']),
+            ])
+            ->overrideByName('ERROR', [
+                new DynamicConfigValue('color', ['mode' => 'fixed', 'fixedColor' => 'red']),
+            ])
+        ;
+    }
+
+    private static function grpcRequestsTimeseries(Service $service): Timeseries\PanelBuilder
+    {
+        return Common::timeseriesPanel()
+            ->title('gRPC Requests')
+            ->unit('reqps')
+            ->withTarget(
+                Common::prometheusQuery("rate(grpc_server_handled_total{service=\"$service->name\"}[\$__rate_interval])")
+                    ->legendFormat('{{ grpc_method }} – {{ grpc_code }}')
             )
-            ->colorScheme((new FieldColorBuilder())->mode(FieldColorModeId::paletteClassic()))
-            ->thresholdsStyle((new GraphThresholdsStyleConfigBuilder())->mode(GraphThresholdsStyleMode::off()))
+            ->datasource(Common::prometheusDatasourceRef())
         ;
     }
 
-    /**
-     * Creates a Prometheus query pre-configured for instant
-     * vectors and table data formatting.
-     * @param string $expression The PromQL query.
-     * @return Prometheus\DataqueryBuilder
-     */
-    public static function instantPrometheusQuery(string $expression): Prometheus\DataqueryBuilder
+    private static function grpcLatenciesHeatmap(Service $service): Heatmap\PanelBuilder
     {
-        return (new Prometheus\DataqueryBuilder())
-            ->expr($expression)
-            ->instant()
-            ->format(PromQueryFormat::table())
-            ->legendFormat('__auto')
+        return Common::heatmapPanel()
+            ->title('gRPC Requests latencies')
+            ->withTarget(
+                Common::prometheusQuery("sum(increase(grpc_server_handling_seconds_bucket{service=\"$service->name\"}[\$__rate_interval])) by (le)")
+                    ->format(PromQueryFormat::heatmap())
+            )
+            ->datasource(Common::prometheusDatasourceRef())
         ;
     }
 
-    /**
-     * Creates a Prometheus query pre-configured for range
-     * vectors.
-     * @param string $expression The PromQL query.
-     * @return Prometheus\DataqueryBuilder
-     */
-    public static function prometheusQuery(string $expression): Prometheus\DataqueryBuilder
+    private static function grpcLogsPanel(Service $service): Logs\PanelBuilder
     {
-        return (new Prometheus\DataqueryBuilder())
-            ->expr($expression)
-            ->range()
-            ->format(PromQueryFormat::timeSeries())
-            ->legendFormat('__auto')
+        return Common::logPanel()
+            ->title('gRPC Logs')
+            ->withTarget(
+                Common::lokiQuery("{service=\"$service->name\", source=\"grpc\", level=~\"\$logs_level\"} |~ \"\$logs_filter\"")
+            )
         ;
     }
 
-    /**
-     * creates a Loki query pre-configured for range vectors.
-     * @param string $expression The LogQL query.
-     * @return Loki\DataqueryBuilder
-     */
-    public static function lokiQuery(string $expression): Loki\DataqueryBuilder
+    private static function httpRequestsTimeseries(Service $service): Timeseries\PanelBuilder
     {
-        return (new Loki\DataqueryBuilder())
-            ->expr($expression)
-            ->QueryType('range')
-            ->legendFormat('__auto')
+        return Common::timeseriesPanel()
+            ->title('HTTP Requests')
+            ->unit('reqps')
+            ->withTarget(
+                Common::prometheusQuery("rate(http_requests_total{service=\"$service->name\"}[\$__rate_interval])")
+                    ->legendFormat('{{code}} – {{ method }} {{ path }}')
+            )
+            ->datasource(Common::prometheusDatasourceRef())
+        ;
+    }
+
+    private static function httpLatenciesHeatmap(Service $service): Heatmap\PanelBuilder
+    {
+        return Common::heatmapPanel()
+            ->title('HTTP Requests latencies')
+            ->withTarget(
+                Common::prometheusQuery("sum(increase(http_requests_duration_seconds_bucket{service=\"$service->name\"}[\$__rate_interval])) by (le)")
+                    ->format(PromQueryFormat::heatmap())
+            )
+            ->datasource(Common::prometheusDatasourceRef())
+        ;
+    }
+
+    private static function httpLogsPanel(Service $service): Logs\PanelBuilder
+    {
+        return Common::logPanel()
+            ->title('HTTP Logs')
+            ->withTarget(
+                Common::lokiQuery("{service=\"$service->name\", source=\"http\", level=~\"\$logs_level\"} |~ \"\$logs_filter\"")
+            )
+        ;
+    }
+
+    private static function logLevelsVariable(Service $service): SDKDashboard\QueryVariableBuilder
+    {
+        return (new SDKDashboard\QueryVariableBuilder('logs_level'))
+            ->label('Logs level')
+            ->datasource(Common::lokiDatasourceRef())
+            ->query([
+				'label'  => 'level',
+				'stream' => "{service=\"$service->name\"}",
+				'type'   =>   1,
+            ])
+            ->refresh(VariableRefresh::onTimeRangeChanged())
+            ->includeAll(true)
+            ->allValue('.*')
         ;
     }
 }
